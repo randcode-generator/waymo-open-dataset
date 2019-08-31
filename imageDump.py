904cad4063d84006943e4d38402f23ca60ca2ee2
Eric <tang8156@gmail.com>
2019-08-31 01:57:25 -0400

    WIP

--- imageDump.py
+++ imageDump.py
@@ -4,6 +4,24 @@ import tensorflow as tf
 import math
 import numpy as np
 import itertools
+from PIL import Image
+from PIL import ImageDraw
+
+
+def find1(item1, tofind):
+  for i in range(len(item1)):
+    print(item1[i].name, tofind)
+    if(item1[i].name == tofind):
+      return item1[i]
+  return nil
+
+def filename(index):
+  if(index < 10):
+    return "00"+str(index)
+  elif(index < 100):
+    return "0"+str(index)
+  else:
+    return str(index)
 
 os.environ['PYTHONPATH']='/env/python:/home/eric/waymo-od'
 m=imp.find_module('waymo_open_dataset', ['/home/eric/waymo-od'])
@@ -38,8 +56,30 @@ for data in dataset:
           except OSError:
             print ("Creation of the directory %s failed" % path)
 
-        fname = tf.constant(path + "/" + str(i)+str(index)+'.jpg')
-        print(path + "/" + str(i)+str(index)+'.jpg')
+        path = path + "/" + filename(i)+'.jpg'
+        fname = tf.constant(path)
+        print(path)
         fwrite = tf.io.write_file(fname, image.image)
         sess.run(fwrite)
-          
+
+        tmp1 = find1(frame.projected_lidar_labels, image.name).labels
+        if(len(tmp1) == 0):
+          continue
+      
+        image = Image.open(path).convert('RGB')
+        draw = ImageDraw.Draw(image)
+        for index1 in range(len(tmp1)):
+          tmp = tmp1[index1].box
+
+          center_x = tmp.center_x
+          center_y = tmp.center_y
+          width = tmp.width
+          length = tmp.length
+
+          left = center_x - (length/2)
+          top = center_y - (width/2)
+          right = center_x + (length/2)
+          bottom = center_y + (width/2)
+          draw.line([(left, top), (left, bottom), (right, bottom), (right, top), (left, top)], width=4, fill='red')
+        
+        image.save(path, format='JPEG')
